74bf9c3fa2bf329547fd8010515fc8c62e11c43e
erdogandemirtas28-wq <erdogandemirtas28@icloud.com>
2026-01-19 20:35:35 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,34 +3,30 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-// Web sayfasını (index.html) internete sunar
 app.use(express.static(__dirname));
 
 let players = {};
 
 io.on('connection', (socket) => {
-    console.log('Yeni oyuncu bağlandı: ' + socket.id);
-
-    // Yeni oyuncu verisi
+    // Yeni oyuncuyu ekranın ortasında başlat
     players[socket.id] = {
-        x: 300, 
-        y: 300, 
-        id: socket.id, 
+        x: 400,
+        y: 300,
+        id: socket.id,
         color: '#' + Math.floor(Math.random()*16777215).toString(16)
     };
     
-    // Mevcut oyuncuları gönder
-    socket.emit('currentPlayers', players);
-    socket.broadcast.emit('newPlayer', players[socket.id]);
+    io.emit('currentPlayers', players);
 
-    // Hareket sinyali geldiğinde
     socket.on('playerInput', (input) => {
-        if (players[socket.id]) {
-            if (input.left) players[socket.id].x -= 5;
-            if (input.right) players[socket.id].x += 5;
-            if (input.up) players[socket.id].y -= 5;
-            if (input.down) players[socket.id].y += 5;
-            io.emit('playerMoved', players[socket.id]);
+        const player = players[socket.id];
+        if (player) {
+            const speed = 7; // Hız artırıldı
+            if (input.left) player.x -= speed;
+            if (input.right) player.x += speed;
+            if (input.up) player.y -= speed;
+            if (input.down) player.y += speed;
+            io.emit('playerMoved', player);
         }
     });
 
@@ -40,8 +36,7 @@ io.on('connection', (socket) => {
     });
 });
 
-// Render için kritik: PORT ayarı
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log("Sunucu " + PORT + " portunda aktif.");
+    console.log("Server running on port " + PORT);
 });
